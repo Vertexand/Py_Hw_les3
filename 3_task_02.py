4ff5115df35eb24f0b0d327365ac64bdf7c7e295
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-10-08 23:32:08 +0300

    add task1

--- 3_task_02.py
+++ 3_task_02.py
@@ -4,16 +4,16 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-# my_list = [2, 3, 4, 5, 6] #зад 
-# res_list = []
+my_list = [2, 3, 4, 5, 6] #вар 1 
+res_list = []
 
-# for i in range(len(my_list)//2):
-#     res_list.append(my_list[i]*my_list[-i-1])
+for i in range(len(my_list)//2):
+    res_list.append(my_list[i]*my_list[-i-1])
 
-# if len(my_list)%2 != 0:
-#     res_list.append(my_list[len(my_list)//2] ** 2)
+if len(my_list)%2 != 0:
+    res_list.append(my_list[len(my_list)//2] ** 2)
     
-# print(res_list)
+print(res_list)
 
 
 # from random import sample # вар 2
@@ -47,66 +47,9 @@
 
 
 
-# import math
-# list = [2, 3, 4, 5, 6,7]
-# result = []              
-
-# n = math.ceil(len(list) / 2)                 #floor округление к меньшему #ceil округление к большему
-# for i in range(n):
-#     result.append(list[i] * list[-i - 1])
-# print(result)
-
-# from random import randrange
-
-# def multiplication_of_pairs(new_list):
-#     right_side = new_list[int(len(new_list)/2):]
-#     right_side.reverse()
-#     if len(new_list)%2 !=0:
-#         left_side = new_list[:int(len(new_list)/2)+1]
-#     else:
-#         left_side = new_list[:int(len(new_list)/2)]
-#     result_list = [l_value*r_value for l_value, r_value in zip(left_side, right_side)]   
-#     return result_list
 
 
-# new_list = [value for value in range(1, randrange(4,20))]
-# print(new_list)
-# print(multiplication_of_pairs(new_list))
 
-
-# from random import randrange# вар из семир чел
-
-# def multiplication_of_pairs(new_list):
-#     right_side = new_list[int(len(new_list)/2):]
-#     right_side.reverse()
-#     if len(new_list)%2 !=0:
-#         left_side = new_list[:int(len(new_list)/2)+1]
-#     else:
-#         left_side = new_list[:int(len(new_list)/2)]
-#     result_list = [l_value*r_value for l_value, r_value in zip(left_side, right_side)] #генератор списка  зип сшивает два массива  
-#     return result_list
-
-
-# new_list = [value for value in range(1, randrange(4,20))]
-# print(new_list)
-# print(multiplication_of_pairs(new_list))
-# n=[1,2,3,4,5,6,7]
-# print(n[4:1:-1])
-# print(n[::-1])
-# print(n[1:6])# 
-
-# print([0 for i in range(23)]) #все 0
-
-my_list = [2, 3, 4, 5, 1] # задача сем
-res_list = []
-half_steps = len(my_list)//2
-
-if len(my_list)%2 != 0:
-    half_steps +=1
-for i in range(half_steps):
-    res_list.append(my_list[i]*my_list[-i-1])
-
-print(res_list)
 
 
 
